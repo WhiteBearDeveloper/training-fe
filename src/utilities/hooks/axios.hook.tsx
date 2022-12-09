d6343fdde6f205e9192de1bce7786fc26c7f925d
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2022-12-10 00:15:45 +0700

    feat: hooks - create hook for api

---
+++ src/utilities/hooks/axios.hook.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+
+interface Props {
+  url: string;
+  method: string;
+  payload: any;
+}
+
+export const useAxios = ({ url, method, payload }: Props) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const controllerRef = useRef(new AbortController());
+  const cancel = () => {
+    controllerRef.current.abort();
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.request({
+          data: payload,
+          signal: controllerRef.current.signal,
+          method,
+          url,
+        });
+
+        setData(response.data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setError(error.message);
+        }
+      } finally {
+        setLoaded(true);
+      }
+    })();
+  }, []);
+
+  return { cancel, data, error, loaded };
+};
