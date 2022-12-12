2bfbf80f7b8b617bd0592f52e607a38419722f8c
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2022-12-12 22:09:38 +0700

    feat: auth - expand auth token getter

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,6 +2,6 @@ export const setAuthToken = (token: string) => {
   localStorage.setItem("authToken", token);
 };
 
-export const getAuthToken = () => {
-  localStorage.getItem("authToken");
+export const getAuthToken = (): string | null => {
+  return localStorage.getItem("authToken");
 };
