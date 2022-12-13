494c1e756b2069843fcadec484978927f9f4bbf6
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2022-12-14 00:39:18 +0700

    refactor: api - refactored controller hook

---
+++ src/api/hooks/controller.hook.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import axios, { Method } from "axios";
+import { apiController } from "@api/controller";
+
+export const useApiController = <T, B>(
+  url: string,
+  method: Method,
+  payload?: B
+) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState("");
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await apiController<T>({
+          payload,
+          method,
+          url,
+        });
+
+        setData(response.data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setError(error.message);
+        }
+      } finally {
+        setLoaded(true);
+      }
+    })();
+  }, []);
+
+  return { data, error, loaded };
+};
