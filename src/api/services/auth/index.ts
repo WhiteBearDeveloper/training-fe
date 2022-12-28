aef231411b25218ed0fd28cb2b74aa7debc7da54
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2022-12-28 21:56:37 +0700

    feat: auth - integrate common auth interface from training logic package

--- src/api/services/auth/index.ts
+++ src/api/services/auth/index.ts
@@ -1,50 +1,46 @@
-import { getLoginEndpoint, getRegistrationEndpoint } from '@api/endpoints/auth'
-import { commonApiService } from '../common'
-import { setAuthToken } from '@services/auth'
-import { $userStore } from '@store/user'
-import { AuthTypes } from './types'
-import { $notificationsStore } from '@store/notifications'
-
-interface SetAuthServicePayload {
-  email: string
-  password: string
-}
+import { getLoginEndpoint, getRegistrationEndpoint } from "@api/endpoints/auth";
+import { commonApiService } from "../common";
+import { setAuthToken } from "@services/auth";
+import { $userStore } from "@store/user";
+import { AuthTypes } from "./types";
+import { $notificationsStore } from "@store/notifications";
+import { AuthProps } from "@whitebeardeveloper/training-logic/src/auth/types";
 
 interface AuthAnswer {
-  id: number
-  token: string
+  id: number;
+  token: string;
 }
 interface SetAuthService {
-  payload: SetAuthServicePayload
-  type: AuthTypes
+  payload: AuthProps;
+  type: AuthTypes;
 }
 
 const getAuthEndpoint = (type: AuthTypes) => {
   switch (type) {
-    case 'registration':
-      return getRegistrationEndpoint()
-    case 'login':
-      return getLoginEndpoint()
+    case "registration":
+      return getRegistrationEndpoint();
+    case "login":
+      return getLoginEndpoint();
   }
-}
+};
 
 export const setAuthService = async ({ payload, type }: SetAuthService) => {
   try {
-    const response = await commonApiService<AuthAnswer, SetAuthServicePayload>({
+    const response = await commonApiService<AuthAnswer, AuthProps>({
       url: getAuthEndpoint(type),
-      method: 'POST',
-      payload
-    })
-    setAuthToken(response.data.token)
-    $userStore.activateAuthorizationStatus()
+      method: "POST",
+      payload,
+    });
+    setAuthToken(response.data.token);
+    $userStore.activateAuthorizationStatus();
     $notificationsStore.addNotification({
       text:
-        type === 'registration'
-          ? 'Регистрация успешно завершена'
-          : 'Вы успешно авторизованы',
-      type: 'success'
-    })
+        type === "registration"
+          ? "Регистрация успешно завершена"
+          : "Вы успешно авторизованы",
+      type: "success",
+    });
   } catch (e) {
   } finally {
   }
-}
+};
