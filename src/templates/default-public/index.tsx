077c2eb9e98b39eba579517f548f57ea13796679
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-01-02 02:54:09 +0700

    feat: common - add topline in default template

--- src/templates/default-public/index.tsx
+++ src/templates/default-public/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Notifications } from "@ui";
 import "./default-public.scss";
 import styles from "./default-public.module.scss";
+import grid from "@styles/modules/abstracts/grid.module.scss";
+import classNames from "classnames";
 
 interface Props {
   children?: React.ReactNode;
@@ -11,7 +13,15 @@ export const DefaultPublicTemplate: React.FC<Props> = ({ children }) => {
   return (
     <div className={styles.wrapper}>
       <Notifications />
-      <>{children}</>
+      <div className={classNames(styles.topline, grid.root)}>
+        <div className={classNames(grid.container, styles.toplineInner)}>
+          <div className={styles.toplineName}>Workout</div>
+          <div className={styles.toplineManagment}>Войти</div>
+        </div>
+      </div>
+      <div className={grid.container}>
+        <>{children}</>
+      </div>
     </div>
   );
 };
