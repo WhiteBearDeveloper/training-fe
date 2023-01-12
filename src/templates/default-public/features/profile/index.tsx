60d472013197ce0096a7b5fef254719b25d5775f
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-01-12 23:51:16 +0700

    refactor: profile - remove development info

--- src/templates/default-public/features/profile/index.tsx
+++ src/templates/default-public/features/profile/index.tsx
@@ -1,11 +1,9 @@
 import { Auth } from "@features/auth";
-import { $profileStore } from "@store/profile";
 import { observer } from "mobx-react-lite";
 import React, { useState } from "react";
 
 export const Profile = observer((): JSX.Element => {
   const [isAuthModalOpen, setAuthModalStatus] = useState<boolean>(false);
-  console.log("object :>> ", $profileStore.profile?.id);
 
   return (
     <>
