c52ae8e7bb1690450d2bd6fa2032b83970327cd2
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-01-16 16:19:09 +0700

    feat: training courses - create api for add training course

--- src/screens/training-course/add/index.tsx
+++ src/screens/training-course/add/index.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import { addTrainingCourseService } from "@api/services/training-courses";
+import React, { useRef, useState } from "react";
 
 export const TrainingCourseAddScreen = (): JSX.Element => {
-  return <div>12312312321312</div>;
+  const [name, setName] = useState<string | null>(null);
+  const nameRef = useRef(null);
+  const onSubmitHandler = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+    const nameCurrent = nameRef.current;
+    // @ts-expect-error
+    setName(nameCurrent.value);
+
+    name &&
+      addTrainingCourseService({
+        payload: {
+          name,
+        },
+      });
+  };
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <input
+        type="text"
+        name="name"
+        ref={nameRef}
+        placeholder="Введите название тренировочного курса"
+      />
+      <button type="submit">Сохранить</button>
+    </form>
+  );
 };
