e6af646b091b699f86fbc1d6d560c6797f8ceccf
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-02-01 06:42:48 +0700

    feat(screens): create and styling of main screen

--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TrainingCoursesList } from "@features/training-course";
+import styles from "./main.module.scss";
+import blockStyles from "@styles/modules/abstracts/block.module.scss";
+import titleStyles from "@styles/modules/abstracts/title.module.scss";
+import { observer } from "mobx-react-lite";
+import { $trainingCourseStore } from "@features/training-course/training-course.store";
+import classNames from "classnames";
 
-export const MainScreen = (): JSX.Element => {
-  return <TrainingCoursesList />;
-};
+const Component = observer((): JSX.Element => {
+  const { trainingCourse, myTrainingCourse, update } = $trainingCourseStore;
+  useEffect(() => {
+    !trainingCourse && update();
+  }, [trainingCourse]);
+
+  const blockClass = classNames(blockStyles.block, styles.block);
+
+  return trainingCourse?.length ? (
+    <>
+      <section className={blockClass}>
+        <h1 className={titleStyles.lg}>Тренировочные курсы</h1>
+        <TrainingCoursesList data={trainingCourse} />
+      </section>
+      {myTrainingCourse?.length && (
+        <section className={blockClass}>
+          <h1 className={titleStyles.lg}>Мои тренировочные курсы</h1>
+          <TrainingCoursesList data={myTrainingCourse} />
+        </section>
+      )}
+    </>
+  ) : (
+    <section className={blockStyles.block}>
+      На данный момент нет активных тренировочных курсов
+    </section>
+  );
+});
+
+export const MainScreen = React.memo(Component);
