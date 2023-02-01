e2de6cc0e8f63b913ecc1eb765875722012d15fb
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-02-01 11:22:22 +0700

    style(training-course): styling training course list

--- src/features/training-course/list/index.tsx
+++ src/features/training-course/list/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import gridStyles from "@styles/modules/abstracts/grid.module.scss";
 import { TrainingCourseModel } from "@whitebeardeveloper/training-logic/src/training-course/types";
 import classNames from "classnames";
+import styles from "./list.module.scss";
 
 interface Props {
   data: TrainingCourseModel[];
@@ -17,7 +18,7 @@ const Component = ({ data }: Props): JSX.Element => {
               className={classNames(gridStyles.item, gridStyles["item-3x"])}
               key={`training-list-${item.id}`}
             >
-              {item.name}
+              <div className={styles.item}>{item.name}</div>
             </div>
           ))}
         </div>
