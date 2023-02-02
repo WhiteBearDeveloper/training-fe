7d73fc3098601f4ea54576d69fa27febe397b8cb
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-02-03 00:53:09 +0700

    refactor(router): remove unusable routes

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,4 @@
 export { MainScreen } from "./main";
 export { NotFoundScreen } from "./not-found";
-export { TrainingCourseScreen } from "./training-course";
 export { TrainingCourseAddScreen } from "./training-course/add";
 export { TrainingCourseDetailScreen } from "./training-course/detail";
