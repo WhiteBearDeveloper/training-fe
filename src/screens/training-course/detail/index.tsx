720671a0c2289d591bde3683812244dcf1fac82c
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-02-03 00:11:49 +0700

    feat(training-course): add go back navigation

--- src/screens/training-course/detail/index.tsx
+++ src/screens/training-course/detail/index.tsx
@@ -1,14 +1,35 @@
 import { TrainingCourseModel } from "@whitebeardeveloper/training-logic/src/training-course/types";
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { NavigateFunction, useLocation, useNavigate } from "react-router-dom";
 import blockStyles from "@styles/modules/abstracts/block.module.scss";
 import titleStyles from "@styles/modules/abstracts/title.module.scss";
+import topLineStyles from "@styles/modules/abstracts/top-line.module.scss";
+import { Button } from "@ui";
 
 export const TrainingCourseDetailScreen = (): JSX.Element => {
-  const state: TrainingCourseModel | null = useLocation().state;
+  const state: TrainingCourseModel = useLocation().state;
+  const navigate: NavigateFunction = useNavigate();
+  const goBackHandler: () => void = (): void => {
+    if (window.history.state && window.history.state.idx > 0) {
+      navigate(-1);
+    } else {
+      navigate("/", { replace: true });
+    }
+  };
   return (
-    <div className={blockStyles.block}>
-      <h1 className={titleStyles.lg}>{state?.name}</h1>
-    </div>
+    <>
+      <div className={topLineStyles.line}>
+        <div className={topLineStyles.buttons}>
+          <Button
+            text="Назад"
+            className={topLineStyles.button}
+            onClick={goBackHandler}
+          />
+        </div>
+      </div>
+      <div className={blockStyles.block}>
+        <h1 className={titleStyles.lg}>{state?.name}</h1>
+      </div>
+    </>
   );
 };
