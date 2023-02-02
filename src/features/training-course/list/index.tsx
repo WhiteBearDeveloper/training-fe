c83ee8f34b75fd98d1804d071a33f81b7a3b29f3
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-02-02 18:37:14 +0700

    feat(training-course): add nav link in list component

--- src/features/training-course/list/index.tsx
+++ src/features/training-course/list/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { TrainingCourseModel } from "@whitebeardeveloper/training-logic/src/training-course/types";
 import classNames from "classnames";
 import styles from "./list.module.scss";
+import { NavLink } from "react-router-dom";
 
 interface Props {
   data: TrainingCourseModel[];
@@ -12,14 +13,18 @@ const Component = ({ data }: Props): JSX.Element => {
     <>
       {data.length && (
         <div className={classNames(styles.list)}>
-          {data?.map((item) => (
-            <div
-              className={classNames(styles.block)}
-              key={`training-list-${item.id}`}
-            >
-              <div className={styles.item}>{item.name}</div>
-            </div>
-          ))}
+          {data?.map((item) => {
+            return (
+              <NavLink
+                to={`training-course/${item.id}`}
+                className={classNames(styles.block)}
+                key={`training-list-${item.id}`}
+                state={{ ...item }}
+              >
+                <div className={styles.item}>{item.name}</div>
+              </NavLink>
+            );
+          })}
         </div>
       )}
     </>
