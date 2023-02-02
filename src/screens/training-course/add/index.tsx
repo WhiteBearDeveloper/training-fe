73b417789b62514f97848a47e8361424179d41b2
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-02-03 00:54:50 +0700

    feat(training-course): add go back navigation in add course page

--- src/screens/training-course/add/index.tsx
+++ src/screens/training-course/add/index.tsx
@@ -4,6 +4,8 @@ import blockStyles from "@styles/modules/abstracts/block.module.scss";
 import titleStyles from "@styles/modules/abstracts/title.module.scss";
 import formStyles from "@styles/modules/abstracts/form.module.scss";
 import { Button, FormWrapper, InputText } from "@ui";
+import topLineStyles from "@styles/modules/abstracts/top-line.module.scss";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
 export const TrainingCourseAddScreen = (): JSX.Element => {
   const [name, setName] = useState<string | null>(null);
@@ -21,22 +23,44 @@ export const TrainingCourseAddScreen = (): JSX.Element => {
         },
       });
   };
+
+  const navigate: NavigateFunction = useNavigate();
+
+  const goBackHandler: () => void = (): void => {
+    if (window.history.state && window.history.state.idx > 0) {
+      navigate(-1);
+    } else {
+      navigate("/", { replace: true });
+    }
+  };
+
   return (
-    <div className={blockStyles.block}>
-      <h1 className={titleStyles.lg}>Создать тренировочный курс</h1>
-      <FormWrapper onSubmit={onSubmitHandler}>
-        <div className={formStyles.row}>
-          <InputText
-            type="text"
-            placeholder="Введите название курса"
-            ref={nameRef}
-            name="name"
+    <>
+      <div className={topLineStyles.line}>
+        <div className={topLineStyles.buttons}>
+          <Button
+            text="Назад"
+            className={topLineStyles.button}
+            onClick={goBackHandler}
           />
         </div>
-        <div className={formStyles.row}>
-          <Button type="submit" text="Сохранить" />
-        </div>
-      </FormWrapper>
-    </div>
+      </div>
+      <div className={blockStyles.block}>
+        <h1 className={titleStyles.lg}>Создать тренировочный курс</h1>
+        <FormWrapper onSubmit={onSubmitHandler}>
+          <div className={formStyles.row}>
+            <InputText
+              type="text"
+              placeholder="Введите название курса"
+              ref={nameRef}
+              name="name"
+            />
+          </div>
+          <div className={formStyles.row}>
+            <Button type="submit" text="Сохранить" />
+          </div>
+        </FormWrapper>
+      </div>
+    </>
   );
 };
