c588f78115b9b6bd5a63423081e13235a9930124
WhiteBearDeveloper <viper010790@mail.ru>
2023-03-06 14:44:10 +0700

    feat: forms - added form validation phrases

---
+++ src/utils/constants/phrases.ts
@@ -0,0 +1,13 @@
+export const $FORM_PHRASES = {
+  common: { required: "Поле обязательно для заполнения" },
+  email: { format: "Введите корректный email" },
+  password: {
+    min_characters_pre: "Пароль не должен быть короче",
+    min_characters_post: "cимволов",
+    should_include_min: "Пароль должен включать не менее",
+    lowercase: "прописных латинских букв",
+    uppercase: "заглавных латинских букв",
+    digits: "цифр",
+    symbols: "символов",
+  },
+};
