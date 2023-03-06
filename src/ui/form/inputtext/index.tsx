262e43b3dbf491471d9b2755a0b1077f7fe81697
WhiteBearDeveloper <viper010790@mail.ru>
2023-03-06 14:46:53 +0700

    feat: ui - integrated react hook form in inputtext component

--- src/ui/form/inputtext/index.tsx
+++ src/ui/form/inputtext/index.tsx
@@ -1,24 +1,47 @@
 import React, { InputHTMLAttributes } from "react";
 import formStyles from "./../form.module.scss";
 import classNames from "classnames";
+import {
+  FieldError,
+  FieldValues,
+  Path,
+  UseFormRegister,
+} from "react-hook-form";
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+interface Props<T extends FieldValues>
+  extends InputHTMLAttributes<HTMLInputElement> {
+  register: UseFormRegister<T>;
+  error?: FieldError;
+  name: Path<T>;
+}
 
-const Component = (
-  props: Props,
-  ref: React.MutableRefObject<HTMLInputElement>,
+export const InputText = <T extends FieldValues>(
+  props: Props<T>,
 ): JSX.Element => {
-  const itemClass = classNames(formStyles.item, formStyles.error);
+  const itemClass = classNames(
+    formStyles.item,
+    props.error && formStyles.error,
+  );
+
   const inputClass = classNames(formStyles.text, props.className);
+  const placeholderClass = classNames(
+    formStyles.placeholder,
+    formStyles["placeholder--fixed"],
+  );
 
   return (
     <div className={itemClass}>
-      <div className={formStyles.placeholder}>
+      {props.error && (
+        <div className={formStyles["error-text"]}>{props.error.message}</div>
+      )}
+      <div className={placeholderClass}>
         {props.placeholder && `${props.placeholder}`}
       </div>
-      <input {...props} className={inputClass} ref={ref} />
+      <input
+        {...props}
+        {...(props.register && props.name && { ...props.register(props.name) })}
+        className={inputClass}
+      />
     </div>
   );
 };
-// @ts-expect-error
-export const InputText = React.forwardRef<HTMLInputElement, Props>(Component);
