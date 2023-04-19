9246d49494a35d463d22c9d09034015e7d894a8e
WhiteBearDeveloper <viper010790@mail.ru>
2023-04-19 13:33:30 +0700

    feat: ui - added button outline style

--- src/ui/button/index.tsx
+++ src/ui/button/index.tsx
@@ -2,22 +2,26 @@ import React, { ButtonHTMLAttributes } from "react";
 import styles from "./button.module.scss";
 import classNames from "classnames";
 
-type Themes = "Main";
+type Themes = "main";
+type Styles = "fill" | "outline";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
   theme?: Themes;
+  buttonStyle?: Styles;
 }
 
 export const Button: React.FC<Props> = ({
   text,
-  theme = "Main",
+  theme = "main",
+  buttonStyle = "fill",
   className,
   ...props
 }): JSX.Element => {
   const blockClassName = classNames(
     styles.block,
-    styles[`block${theme}`],
+    styles[`block-${theme}`],
+    buttonStyle === "fill" ? styles.fill : styles.outline,
     className,
   );
   return (
