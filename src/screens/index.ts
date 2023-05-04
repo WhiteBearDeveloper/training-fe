be3a3dee292098b03ec1f9e4cf88cfb2fc54b81d
WhiteBearDeveloper <viper010790@mail.ru>
2023-05-04 23:15:33 +0700

    feat: routes - moved not found screen to special screens folder

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,5 @@
 export { MainScreen } from "./main";
-export { NotFoundScreen } from "./not-found";
+export { NotFoundScreen } from "./special-screens/not-found";
 export { NoAccessScreen } from "./no-access";
 export { TrainingCourseAddScreen } from "./training-course/add";
 export { TrainingCourseDetailScreen } from "./training-course/detail";
