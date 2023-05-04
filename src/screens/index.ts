74c08983324bfd640d241ac7a82899a7ce8f6394
WhiteBearDeveloper <viper010790@mail.ru>
2023-05-04 23:16:36 +0700

    feat: screens - created error network screen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,3 +3,4 @@ export { NotFoundScreen } from "./special-screens/not-found";
 export { NoAccessScreen } from "./no-access";
 export { TrainingCourseAddScreen } from "./training-course/add";
 export { TrainingCourseDetailScreen } from "./training-course/detail";
+export { ErrorNetworkScreen } from "./special-screens/error-network";
---
+++ src/screens/special-screens/error-network/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ErrorNetworkScreen = (): JSX.Element => {
+  return (
+    <div>
+      Нет соединения с сервером, проверьте Интернет соединение или попробуйте
+      позже
+    </div>
+  );
+};
